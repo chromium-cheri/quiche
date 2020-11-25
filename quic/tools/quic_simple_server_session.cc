fc3aa0e01319ebc5a34df47bd575392fb06fd93d
LiuJiyong <liujiyong4@gmail.com>
2020-11-25 21:04:21 +0800

    Fix crash about array out of bounds When receiving an RstStreamFrame with an error stream_id, for example, an even stream_id, or a larger one that exceeds the limit of max_open_incoming_streams_, the server will enter HandleRstOnValidNonexistentStream. When stream_id is a larger value, index must be greater than promised_streams_.size(), which causes the array to be out of bounds and the service crash.
    This wrong RstStreamFrame is easy to construct and may be used by attackers
    Just add one line of code to construct a malicious client
    ```
    -- a/chromium/src/net/third_party/quiche/src/quic/tools/quic_toy_client.cc
    +++ b/chromium/src/net/third_party/quiche/src/quic/tools/quic_toy_client.cc
    @@ -413,6 +413,7 @@ int QuicToyClient::SendRequestsAndPrintResponses(
           return 1;
         }
    
    +    client->session()->SendRstStream(400, QUIC_REFUSED_STREAM, 0);
         int response_code = client->latest_response_code();
         if (response_code >= 200 && response_code < 300) {
           std::cout << "Request succeeded (" << response_code << ")." << std::endl;
    @@ -438,7 +439,6 @@ int QuicToyClient::SendRequestsAndPrintResponses(
           }
         }
       }
    ```
    
    Maybe it is a better way to modify the IsClosedStream function, but IsClosedStream is called in many places and cannot be easily modified.
    
    Change-Id: I96fc1c013a62e9b57ae129b4777a11be74f512fa

--- quic/tools/quic_simple_server_session.cc
+++ quic/tools/quic_simple_server_session.cc
@@ -164,7 +164,9 @@ void QuicSimpleServerSession::HandleRstOnValidNonexistentStream(
     size_t index = (frame.stream_id - next_stream_id) /
                    QuicUtils::StreamIdDelta(transport_version());
     DCHECK_LE(index, promised_streams_.size());
-    promised_streams_[index].is_cancelled = true;
+    if (index < promised_streams_.size()) {
+      promised_streams_[index].is_cancelled = true;
+    }
     control_frame_manager().WriteOrBufferRstStream(frame.stream_id,
                                                    QUIC_RST_ACKNOWLEDGEMENT, 0);
     connection()->OnStreamReset(frame.stream_id, QUIC_RST_ACKNOWLEDGEMENT);
